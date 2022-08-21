c44f15a79724e34ca0ad7244caba2ca3c06caf75
V11bot <111274955+V11bot@users.noreply.github.com>
2022-08-21 16:12:55 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *INDOSAT/DANA:* [${global.ppulsa}]
+│ • *AXIS/DANA:* [${global.ppulsa}]
 
 `
 const templateButtons = [
